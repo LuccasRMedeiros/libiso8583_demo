02819492d5519c50975c577eed5a9601a638eec6
Luccas Rocigno de Medeiros <luccasrocigno@hotmail.com>
2025-08-05 22:20:52 -0300

    Creating the unit test class

---
+++ test_cases/unit_test_class/test_case.cpp
@@ -0,0 +1,52 @@
+#include "test_case.hpp"
+
+#include <iostream>
+
+#define TITLE_FORMAT    "\e[1;34m"
+#define SUCCESS_FORMAT  "\e[1;32m"
+#define ERROR_FORMAT    "\e[1;31m"
+#define RESET_FORMAT    "\e[0m"
+
+SimpleUnitTest::SimpleUnitTest(
+        std::string test_title,
+        std::function<bool(void)> test_case)
+{
+    _test_title = test_title;
+    _test_case = test_case;
+}
+
+bool SimpleUnitTest::run(void)
+{
+    std::cout << TITLE_FORMAT << _test_title << ": ";
+
+    if (_test_case() == true)
+    {
+        std::cout << SUCCESS_FORMAT << "OK!" << RESET_FORMAT << std::endl;
+        return true;
+    }
+
+    std::cout << ERROR_FORMAT << "FAIL." << RESET_FORMAT << std::endl;
+    return false;
+
+}
+
+SimpleTest::SimpleTest()
+{
+    _success_count = 0;
+    _error_count = 0;
+}
+
+void SimpleTest::run_tests(void)
+{
+    std::cout << RESET_FORMAT << "Number of tests: " << vec_unit_tests.size() << std::endl;
+    
+    for (unsigned int test_n = 0; test_n < vec_unit_tests.size(); ++test_n)
+    {
+        if (vec_unit_tests[test_n].run() == true)
+            _success_count += 1;
+        else
+            _error_count += 1;
+    }
+
+    std::cout << RESET_FORMAT << "Finished, success count: " << _success_count << ", failures: " << _error_count << std::endl;
+}
---
+++ test_cases/unit_test_class/test_case.hpp
@@ -0,0 +1,32 @@
+#ifndef TEST_CASE_HPP
+#define TEST_CASE_HPP
+
+#include <functional>
+#include <string>
+#include <vector>
+
+class SimpleUnitTest
+{
+    private:
+        std::function<bool(void)> _test_case;
+        std::string _test_title;
+
+    public:
+        SimpleUnitTest(std::string test_title, std::function<bool(void)> test_case);
+
+        bool run(void);
+};
+
+class SimpleTest 
+{
+    private:
+        unsigned int _success_count;
+        unsigned int _error_count;
+
+    public:
+        std::vector<SimpleUnitTest> vec_unit_tests;
+
+        void run_tests(void);
+};
+
+#endif
