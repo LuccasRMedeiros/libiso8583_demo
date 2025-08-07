a5251863f4a1bd7cb2b54cba0a884cd89799e9ce
Luccas Rocigno de Medeiros <luccas.medeiros@gertec.com.br>
2025-08-07 17:47:55 -0300

    Polishing and advancing with the flash memory simulator

--- lib/src/memory.c
+++ lib/src/memory.c
@@ -3,7 +3,8 @@
 #include <string.h>
 
 static byte memory_disk[MEMORY_SIZE];
-static memory_disk_accessors disk_access;
+static memory_disk_accessors_st disk_access;
+
 
 void memory_init(void)
 {
@@ -11,25 +12,32 @@ void memory_init(void)
 
     memset(memory_disk, 0xff, sizeof (memory_disk));
     disk_access.entry_ptr = memory_disk;
+    disk_access.block_size = BLOCK_SIZE;
+    disk_access.total_blocks = BLOCK_CNT;
+    disk_access.free_bitmap = 0b01111111; // All blocks are suposed to available except block 0 (reserved for filesystem)
 
     for (size_t block_id = 0; block_id < BLOCK_CNT; ++block_id)
     {
         for(size_t page_id = 0; page_id < PAGE_CNT; ++page_id)
         {
             disk_access.block[block_id].page_ptr[page_id] = memory_disk_ptr;
-            memory_disk_ptr += PAGE_SIZE;
+            memory_disk_ptr += PAGE_SIZE; // Equivalent to ((block id * total blocks) + page id) + page size
         }
     }
+
+    // Writes the filesystem on memory
+    memcpy(disk_access.block[0].page_ptr, disk_access.block_size, sizeof (disk_access.block_size));
+    memcpy(disk_access.block[0].page_ptr + sizeof(disk_access.block_size), disk_access.total_blocks, sizeof (disk_access.total_blocks));
+    memcpy(disk_access.block[0].page_ptr + sizeof(disk_access.block_size) + sizeof (disk_access.total_blocks), disk_access.free_bitmap, sizeof (disk_access.free_bitmap));
 }
 
 int memory_erase_block(size_t block_id)
 {
-    if (block_id >= BLOCK_CNT)
-        return -1;
+    if (block_id <= 0 || block_id >= BLOCK_CNT)
+        return -1; // Out of bounds (or want to erase filesystem)
 
     memset(disk_access.block[block_id].page_ptr[0], 0xff, BLOCK_SIZE);
 
     return 0;
 }
 
-int memory_write(
--- lib/src/memory.h
+++ lib/src/memory.h
@@ -1,11 +1,13 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #define MEMORY_SIZE 4096
-#define BLOCK_SIZE  4096
-#define PAGE_SIZE   512
+#define BLOCK_SIZE  512
+#define PAGE_SIZE   256
 #define BLOCK_CNT   MEMORY_SIZE/BLOCK_SIZE
 #define PAGE_CNT    BLOCK_SIZE/PAGE_SIZE
 
@@ -15,11 +17,15 @@ typedef struct
 {
     byte *entry_ptr;
 
+    uint16_t block_size;
+    uint8_t total_blocks;
+    uint8_t free_bitmap;
+
     struct
     {
         byte *page_ptr[PAGE_CNT];
     } block[BLOCK_CNT];
-} memory_disk_accessors;
+} flash_memory_access_st;
 
 void memory_init(void);
 int memory_erase_block(size_t block_id);
